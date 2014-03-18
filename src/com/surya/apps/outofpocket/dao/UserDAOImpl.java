a38a3028e6f8367a0da2b943f15fe36b2a775624
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 18:35:35 -0700

    findByEmail implemented in UserDAOImpl

--- src/com/surya/apps/outofpocket/dao/UserDAOImpl.java
+++ src/com/surya/apps/outofpocket/dao/UserDAOImpl.java
@@ -1,18 +1,42 @@
 package com.surya.apps.outofpocket.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import com.surya.apps.outofpocket.common.util.Logger;
 import com.surya.apps.outofpocket.po.UserPO;
 
-public class UserDAOImpl extends NamedDAOImpl<UserPO> implements IUserDAO<UserPO> {
+public class UserDAOImpl extends NamedDAOImpl<UserPO> implements
+		IUserDAO<UserPO> {
+	private static final Logger log = Logger.get();
 
 	public UserDAOImpl() {
 		super(UserPO.class);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public UserPO findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Query q = getSession().createQuery(
+				"select po from " + getPOClassName() + " po where email = ?");
+		q.setParameter(1, email);
+
+		@SuppressWarnings("unchecked")
+		List<UserPO> pos = q.getResultList();
+
+		UserPO po = null;
+		if (pos != null && pos.size() != 0) {
+			if (pos.size() > 1) {
+				String message = "More than one user account retrieved with email: "
+						+ email;
+				log.error(message);
+				throw new RuntimeException(message);
+			} else {
+				po = pos.get(0);
+			}
+		}
+
+		return po;
 	}
 
 }
