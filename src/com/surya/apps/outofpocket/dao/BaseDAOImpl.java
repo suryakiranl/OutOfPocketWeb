2538af08f22f6926721b4b4284ae489e1fe7963b
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 18:18:59 -0700

    findById and findAll methods implemented

--- src/com/surya/apps/outofpocket/dao/BaseDAOImpl.java
+++ src/com/surya/apps/outofpocket/dao/BaseDAOImpl.java
@@ -12,56 +12,62 @@ import com.surya.apps.outofpocket.po.BasePO;
 public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
 	private static final Logger log = Logger.get();
 	private BasePO po;
-	
+
 	public BaseDAOImpl(BasePO po) {
 		this.po = po;
 	}
-	
+
 	@Override
 	public PO save(PO po) {
 		log.enter(po);
-		
-		if(po != null) {
+
+		if (po != null) {
 			// If the object already exists in the system
 			// merge changes
-			if(po.getId() != 0) {
+			if (po.getId() != 0) {
 				getSession().merge(po);
 			} else { // Else, create a new object
 				getSession().persist(po);
 			}
+			commit();
 		} else {
 			log.warn("Nothing to save as parameter is NULL.");
 		}
-		
+
 		log.exit(po);
 		return null;
 	}
 
 	@Override
-	public PO findById(long id) {		
+	public PO findById(long id) {
 		log.enter(id);
-		
+
 		PO returnObj;
-		String className = po.getClass().getSimpleName();
-		Query q = getSession().createQuery("select po from " + className + " po where id = ?");
+		Query q = getSession().createQuery(
+				"select po from " + getPOClassName() + " po where id = ?");
 		q.setParameter(1, id);
-		
+
 		@SuppressWarnings("unchecked")
-		List<PO> rs = q.getResultList();
-		
-		if(rs == null || rs.size() == 0) {
+		List<PO> pos = q.getResultList();
+
+		if (pos == null || pos.size() == 0) {
 			returnObj = null;
 		} else {
-			returnObj = rs.get(0);
+			returnObj = pos.get(0);
 		}
-		
+
 		return returnObj;
 	}
 
 	@Override
 	public List<PO> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Query q = getSession().createQuery(
+				"select po from " + getPOClassName() + " po");
+
+		@SuppressWarnings("unchecked")
+		List<PO> pos = q.getResultList();
+
+		return pos;
 	}
 
 	protected final EntityManager getSession() {
@@ -73,8 +79,9 @@ public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
 		if (em.isOpen() && (em.getTransaction() != null)
 				&& (em.getTransaction().isActive())) {
 			em.getTransaction().commit();
+			log.finer("Transaction committed");
 		} else {
-			
+			log.warn("Nothing to commit");
 		}
 	}
 
@@ -83,7 +90,13 @@ public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
 		if (em.isOpen() && (em.getTransaction() != null)
 				&& (em.getTransaction().isActive())) {
 			em.getTransaction().rollback();
+			log.finer("Transaction rolledback");
+		} else {
+			log.warn("Nothing to rollback");
 		}
 	}
 
+	protected final String getPOClassName() {
+		return po.getClass().getSimpleName();
+	}
 }
