541564af5fdcbe924331bc27cddefe1c4f525016
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 18:14:25 -0700

    Code to save and findById in BaseDAOImpl added

--- src/com/surya/apps/outofpocket/dao/BaseDAOImpl.java
+++ src/com/surya/apps/outofpocket/dao/BaseDAOImpl.java
@@ -3,21 +3,59 @@ package com.surya.apps.outofpocket.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
+import com.surya.apps.outofpocket.common.util.Logger;
 import com.surya.apps.outofpocket.dao.util.EMF;
 import com.surya.apps.outofpocket.po.BasePO;
 
 public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
+	private static final Logger log = Logger.get();
+	private BasePO po;
+	
+	public BaseDAOImpl(BasePO po) {
+		this.po = po;
+	}
+	
 	@Override
 	public PO save(PO po) {
-		// TODO Auto-generated method stub
+		log.enter(po);
+		
+		if(po != null) {
+			// If the object already exists in the system
+			// merge changes
+			if(po.getId() != 0) {
+				getSession().merge(po);
+			} else { // Else, create a new object
+				getSession().persist(po);
+			}
+		} else {
+			log.warn("Nothing to save as parameter is NULL.");
+		}
+		
+		log.exit(po);
 		return null;
 	}
 
 	@Override
-	public PO findById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public PO findById(long id) {		
+		log.enter(id);
+		
+		PO returnObj;
+		String className = po.getClass().getSimpleName();
+		Query q = getSession().createQuery("select po from " + className + " po where id = ?");
+		q.setParameter(1, id);
+		
+		@SuppressWarnings("unchecked")
+		List<PO> rs = q.getResultList();
+		
+		if(rs == null || rs.size() == 0) {
+			returnObj = null;
+		} else {
+			returnObj = rs.get(0);
+		}
+		
+		return returnObj;
 	}
 
 	@Override
