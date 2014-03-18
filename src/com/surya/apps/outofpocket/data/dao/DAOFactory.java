d486c6451c45399aa7a6eaf6e1a84e598e0b41f1
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 23:46:57 -0700

    Added DAOFactory class

---
+++ src/com/surya/apps/outofpocket/data/dao/DAOFactory.java
@@ -0,0 +1,15 @@
+package com.surya.apps.outofpocket.data.dao;
+
+import com.surya.apps.outofpocket.data.po.UserPO;
+
+public class DAOFactory {
+	private static final DAOFactory DAO_FACTORY = new DAOFactory();
+	
+	public static final DAOFactory get() {
+		return DAO_FACTORY;
+	}
+	
+	public final IUserDAO<UserPO> getUserDAO() {
+		return new UserDAOImpl();
+	}
+}
