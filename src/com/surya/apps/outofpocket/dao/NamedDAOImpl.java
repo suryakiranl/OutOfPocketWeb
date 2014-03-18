ad18b9a133682161ba2a10a86f19cba51816dcc4
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 18:23:16 -0700

    Logic for NamedDAOImpl

---
+++ src/com/surya/apps/outofpocket/dao/NamedDAOImpl.java
@@ -0,0 +1,26 @@
+package com.surya.apps.outofpocket.dao;
+
+import java.util.List;
+
+import javax.persistence.Query;
+
+import com.surya.apps.outofpocket.po.NamedPO;
+
+public class NamedDAOImpl<PO extends NamedPO> extends BaseDAOImpl<PO> implements
+		INamedDAO<PO> {
+	public NamedDAOImpl(NamedPO po) {
+		super(po);
+	}
+
+	@Override
+	public List<PO> findByName(String name) {
+		Query q = getSession().createQuery(
+				"select po from " + getPOClassName() + " po where name = ?");
+		q.setParameter(1, name);
+
+		@SuppressWarnings("unchecked")
+		List<PO> pos = q.getResultList();
+
+		return pos;
+	}
+}
