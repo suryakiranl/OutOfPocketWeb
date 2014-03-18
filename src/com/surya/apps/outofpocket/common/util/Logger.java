8644b7b53d323dc15a187d7ecd578dee55a05a0a
Surya Kiran <suryakiranl@gmail.com>
2014-03-17 23:46:38 -0700

    Fixed logging class

--- src/com/surya/apps/outofpocket/common/util/Logger.java
+++ src/com/surya/apps/outofpocket/common/util/Logger.java
@@ -3,10 +3,14 @@ package com.surya.apps.outofpocket.common.util;
 import java.util.logging.Level;
 
 public final class Logger {
-	private static final java.util.logging.Logger LOG = java.util.logging.Logger
-			.getLogger("");
-
-	private static final Logger logger = new Logger();
+	private static final java.util.logging.Logger LOG;
+	private static final Logger logger;
+	
+	static {
+		LOG = java.util.logging.Logger.getLogger("");
+		LOG.setLevel(Level.ALL);
+		logger = new Logger();
+	}
 
 	public static final Logger get() {
 		return logger;
@@ -43,18 +47,22 @@ public final class Logger {
 	}
 
 	public void enter(Object... obj) {
-		debug("Inside method. Params[ " + obj + " ]");
+		if (LOG.isLoggable(Level.FINE)) {
+			LOG.fine(getLogPrefix() + " > Inside method. Params[ " + obj.toString() + " ]");
+		}
 	}
 
 	public void exit(Object... obj) {
-		debug("Exiting method. Params[ " + obj + " ]");
+		if (LOG.isLoggable(Level.FINE)) {
+			LOG.fine(getLogPrefix() + " > Exiting method. Params[ " + obj.toString() + " ]");
+		}
 	}
 
 	private String getLogPrefix() {
 		StackTraceElement[] s = new RuntimeException().getStackTrace();
 		StringBuffer sb = new StringBuffer();
-		sb.append(s[1].getClassName()).append(" - ")
-				.append(s[1].getMethodName());
+		sb.append(s[2].getClassName()).append(" - ")
+				.append(s[2].getMethodName());
 		return sb.toString();
 	}
 }
