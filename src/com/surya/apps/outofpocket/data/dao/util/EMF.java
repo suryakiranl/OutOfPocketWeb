7c3b6dbf173d5ed276fb8f8cd4d557721f83c86b
Surya Kiran <suryakiranl@gmail.com>
2014-04-13 16:00:59 -0700

    Code review - Add code comments

--- src/com/surya/apps/outofpocket/data/dao/util/EMF.java
+++ src/com/surya/apps/outofpocket/data/dao/util/EMF.java
@@ -3,14 +3,29 @@ package com.surya.apps.outofpocket.data.dao.util;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Entity Manger Factory class to fetch and retrieve Entity Manager Factory
+ * instance from the Persistence object.
+ * 
+ * NOTE: This uses the default Entity Manger Factory name: out-of-office-web
+ * 
+ * @author surya
+ * 
+ */
 public final class EMF {
 	private static final String EMF_NAME = "out-of-office-web";
-    private static final EntityManagerFactory emfInstance =
-        Persistence.createEntityManagerFactory(EMF_NAME);
+	private static final EntityManagerFactory emfInstance = Persistence
+			.createEntityManagerFactory(EMF_NAME);
 
-    private EMF() {}
+	private EMF() {
+	}
 
-    public static EntityManagerFactory get() {
-        return emfInstance;
-    }
+	/**
+	 * Singleton instance of the class to obtain a copy.
+	 * 
+	 * @return - EntityMangerFactory instance.
+	 */
+	public static EntityManagerFactory get() {
+		return emfInstance;
+	}
 }
