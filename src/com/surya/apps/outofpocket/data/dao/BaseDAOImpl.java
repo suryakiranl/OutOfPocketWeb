6e2286922fe0dd204bf4add40758806bd1961c57
Surya Kiran <suryakiranl@gmail.com>
2014-04-13 14:40:21 -0700

    Incorporating code review comments
    Updated BaseDAOImpl to include code review comments

--- src/com/surya/apps/outofpocket/data/dao/BaseDAOImpl.java
+++ src/com/surya/apps/outofpocket/data/dao/BaseDAOImpl.java
@@ -21,26 +21,26 @@ public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
 	public PO save(PO po) {
 		log.enter(po);
 
-		if (po != null) {
-			EntityManager em = EMF.get().createEntityManager();
-			try {
-				em.getTransaction().begin();
-				// If the object already exists in the system
-				// merge changes
-				if (po.getId() != null && po.getId() != 0) {
-					em.merge(po);
-				} else { // Else, create a new object
-					em.persist(po);
-				}
-				em.getTransaction().commit();
-			} finally {
-				if (em.getTransaction().isActive()) {
-			        em.getTransaction().rollback();
-			    }
-				em.close();
+		if (po == null) {
+			log.warn("Nothing to save as PO is NULL.");
+			return null;
+		}
+
+		EntityManager em = getEntityManager();
+		try {
+			em.getTransaction().begin();
+			// If the object already exists in the system
+			// merge changes
+			if (po.getId() != null && po.getId() != 0) {
+				em.merge(po);
+			} else { // Else, create a new object
+				em.persist(po);
 			}
-		} else {
-			log.warn("Nothing to save as parameter is NULL.");
+			em.getTransaction().commit();
+		} finally {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
 		}
 
 		log.exit(po);
@@ -52,46 +52,49 @@ public class BaseDAOImpl<PO extends BasePO> implements IBaseDAO<PO> {
 	public PO findById(long id) {
 		log.enter(id);
 
+		final String findByIdQuery = "select po from " + getPOClassName()
+				+ " po where id = ?";
+
+		Query q = getEntityManager().createQuery(findByIdQuery);
+		q.setParameter(1, id);
+		List<PO> pos = executeQuery(q);
+
 		PO po = null;
-		EntityManager em = EMF.get().createEntityManager();
-		try {
-			Query q = em.createQuery("select po from " + getPOClassName()
-					+ " po where id = ?");
-			q.setParameter(1, id);
-
-			@SuppressWarnings("unchecked")
-			List<PO> pos = q.getResultList();
-
-			if (pos == null || pos.size() == 0) {
-				po = null;
-			} else {
-				po = pos.get(0);
-			}
-		} finally {
-			// em.close();
+		if (pos != null && pos.size() > 0) {
+			po = pos.get(0);
 		}
 
 		return po;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public List<PO> findAll() {
-		List<PO> pos = null;
-		EntityManager em = EMF.get().createEntityManager();
-		try {
-			Query q = em.createQuery("select po from " + getPOClassName()
-					+ " po");
+		final String findAllQuery = "select po from " + getPOClassName()
+				+ " po";
 
-			pos = q.getResultList();
-		} finally {
-			// em.close();
-		}
+		return executeQuery(findAllQuery);
+	}
+
+	protected List<PO> executeQuery(String queryStr) {
+		Query q = getEntityManager().createQuery(queryStr);
+		@SuppressWarnings("unchecked")
+		List<PO> pos = q.getResultList();
+
+		return pos;
+	}
+
+	protected List<PO> executeQuery(Query q) {
+		@SuppressWarnings("unchecked")
+		List<PO> pos = q.getResultList();
 
 		return pos;
 	}
 
 	protected final String getPOClassName() {
 		return poClass.getSimpleName();
+	}
+	
+	protected final EntityManager getEntityManager() {
+		return EMF.get().createEntityManager();
 	}
 }
