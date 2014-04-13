b04d099a44856fed7ea1481529cc3d666f3594ec
Surya Kiran <suryakiranl@gmail.com>
2014-04-13 15:39:49 -0700

    Attaching custom logging level to diff packages
    Our project will log all content, however others are expected to logs
    at WARNING level

--- src/com/surya/apps/outofpocket/common/util/Logger.java
+++ src/com/surya/apps/outofpocket/common/util/Logger.java
@@ -2,59 +2,93 @@ package com.surya.apps.outofpocket.common.util;
 
 import java.util.logging.Level;
 
+/**
+ * Utility Logger class used by rest of the project
+ * 
+ * @author surya
+ *
+ */
 public final class Logger {
-	private static final java.util.logging.Logger LOG;
-	private static final Logger logger;
-	
-	static {
-		LOG = java.util.logging.Logger.getLogger("");
-		LOG.setLevel(Level.ALL);
-		logger = new Logger();
-	}
+	private static final java.util.logging.Logger LOG = java.util.logging.Logger
+			.getLogger("com.surya");;
+	private static Logger logger;
 
+	/**
+	 * Singleton method to initialize and access Logger.
+	 * 
+	 * @return - Instance of Logger class
+	 */
 	public static final Logger get() {
+		if (logger == null) {
+			logger = new Logger();
+		}
 		return logger;
 	}
 
+	/**
+	 * Utility method to log a message at FINE level.
+	 * 
+	 * @param obj - Content to log.
+	 */
 	public void debug(Object obj) {
 		if (LOG.isLoggable(Level.FINE)) {
-			LOG.fine(getLogPrefix() + " > " + obj);
+			LOG.fine(getLogPrefix() + " > " + getString(obj));
 		}
 	}
 
+	/**
+	 * Utility method to log a message at INFO level.
+	 * 
+	 * @param obj - Content to log
+	 */
 	public void info(Object obj) {
 		if (LOG.isLoggable(Level.INFO)) {
-			LOG.info(getLogPrefix() + " > " + obj);
+			LOG.info(getLogPrefix() + " > " + getString(obj));
 		}
 	}
 
 	public void error(Object obj) {
 		if (LOG.isLoggable(Level.SEVERE)) {
-			LOG.severe(getLogPrefix() + " > " + obj);
+			LOG.severe(getLogPrefix() + " > " + getString(obj));
 		}
 	}
 
 	public void warn(Object obj) {
 		if (LOG.isLoggable(Level.WARNING)) {
-			LOG.warning(getLogPrefix() + " > " + obj);
+			LOG.warning(getLogPrefix() + " > " + getString(obj));
 		}
 	}
 
 	public void finer(Object obj) {
 		if (LOG.isLoggable(Level.FINER)) {
-			LOG.finer(getLogPrefix() + " > " + obj);
+			LOG.finer(getLogPrefix() + " > " + getString(obj));
 		}
 	}
 
 	public void enter(Object... obj) {
 		if (LOG.isLoggable(Level.FINE)) {
-			LOG.fine(getLogPrefix() + " > Inside method. Params[ " + obj.toString() + " ]");
+			LOG.fine(getLogPrefix() + " > Inside method. Params[ "
+					+ getString(obj) + " ]");
 		}
+	}
+
+	private final String getString(Object... obj) {
+		StringBuffer objValue = new StringBuffer();
+
+		if (obj != null) {
+			for (Object o : obj) {
+				objValue.append(o).append(" - ");
+			}
+		}
+
+		return objValue.toString();
 	}
 
 	public void exit(Object... obj) {
 		if (LOG.isLoggable(Level.FINE)) {
-			LOG.fine(getLogPrefix() + " > Exiting method. Params[ " + obj.toString() + " ]");
+			LOG.fine(getLogPrefix() + " > Exiting method. Params[ "
+					+ getString(obj) + " ]");
+
 		}
 	}
 
